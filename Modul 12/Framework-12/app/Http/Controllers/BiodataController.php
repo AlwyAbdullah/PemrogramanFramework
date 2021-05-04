102c1281c62516a9967134ff06fac8119e3b7e6d
AlwyAbdullah <54932526+AlwyAbdullah@users.noreply.github.com>
2021-05-04 11:45:23 +0700

    Add files via upload

---
+++ Modul 12/Framework-12/app/Http/Controllers/BiodataController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+use illuminate\Http\Request;
+
+class BiodataController extends Controller
+{
+    public function index()
+    {
+        return view('biodata');
+    }
+}
+
+?>
+
